882c1815224134da9e9bc42f657c15335f1cdc3b
administrador <alecurfon@gmail.com>
2019-10-08 14:24:14 +0100

    developing paged search

--- src/app/modules/consult/page/page.component.ts
+++ src/app/modules/consult/page/page.component.ts
@@ -20,14 +20,57 @@ export class ConsultPageComponent {
   constructor(private restfulApi: RestfulService) {}
 
   resultList: Array<any> = [];
+  pages = 1;
+  total = 0;
   form = new FormGroup({
     type : new FormControl('biodatabase'),
-    search : new FormControl('')
+    search : new FormControl(''),
+    page : new FormControl(1),
+    page_size : new FormControl(50)
   });
+
+  serializeItem(object) {
+    var result = '';
+    for (let key in object) {
+      if((key.indexOf('id') > -1) || (object[key] == undefined)) {
+        continue;
+      }
+      result += key + ' : ' + object[key] + ',\t';
+    }
+    return result;
+  }
+
+  changePage(type) {
+    switch(type) {
+      case 'first':
+        this.form.controls['page'].setValue(1);
+        break;
+      case 'prev':
+        this.form.controls['page'].setValue(this.form.value['page'] - 1);
+        break;
+      case 'next':
+        this.form.controls['page'].setValue(this.form.value['page'] + 1);
+        break;
+      case 'last':
+        this.form.controls['page'].setValue(this.pages);
+        break;
+      default:
+        return;
+    }
+    this.onSubmit();
+  }
+
+  search() {
+    this.form.controls['page'].setValue(1);
+    this.pages = 1;
+    this.onSubmit();
+  }
 
   onSubmit() {
     this.restfulApi.search(this.form.value).subscribe(response => {
-      this.resultList = response;
+      this.resultList = response['result'];
+      this.pages = response['pages'];
+      this.total = response['total'];
       console.log(response);
     }, error => {
       console.log(error);
