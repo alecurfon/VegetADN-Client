c04ad65ade1db5c7628bb399c517647c4d40d87a
administrador <alecurfon@gmail.com>
2020-02-29 19:29:13 +0000

    setting up the installer

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt -qq -y install npm
 sudo npm install npm@6.13 -g
 sudo npm install @angular/cli -g
-sudo npm update -g
-printf ">> Executing ..."
+sudo npm update
+#sudo npm install --save-dev @angular-devkit/build-angular
+printf ">> Executing ...\n"
 ng serve --open >/dev/null 2>&1 &
